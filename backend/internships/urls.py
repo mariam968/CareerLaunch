00af9cba3a6ca1b21655ffb59f3a42ba030c1132
mariam968-code <bisirikirwamariam968@gmail.com>
2026-08-24 07:50:18 +0300

    feat: add internship detail API

--- backend/internships/urls.py
+++ backend/internships/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
-from .views import InternshipListView
+from .views import InternshipListView, InternshipDetailView
 
 
 urlpatterns = [
     path('', InternshipListView.as_view(), name='internship-list'),
+
+    path(
+        '<int:pk>/',
+        InternshipDetailView.as_view(),
+        name='internship-detail'
+    ),
 ]
--- backend/internships/views.py
+++ backend/internships/views.py
@@ -7,3 +7,9 @@ from .serializers import InternshipSerializer
 class InternshipListView(generics.ListAPIView):
     queryset = Internship.objects.all().order_by('-created_at')
     serializer_class = InternshipSerializer
+
+
+
+class InternshipDetailView(generics.RetrieveAPIView):
+    queryset = Internship.objects.all()
+    serializer_class = InternshipSerializer
